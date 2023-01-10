933e40b078de40efaab5f9164670afb985d14177
Brandon Whitt <bwhitt@crawford-consulting.com>
2023-01-10 17:21:07 +0000

    message

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zcostcentertest.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
